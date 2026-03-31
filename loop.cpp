a633aa629d5cd57ab11cbc36a6d288a2c1cd040b
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 15:15:57 +0700

    menambahkan return diakhir

--- loop.cpp
+++ loop.cpp
@@ -6,7 +6,12 @@ int angka;
 int pilihan;
 
 // cek bilangan prima
-bool cekPrima(){
+bool cekPrima() {
     if (angka < 2) return false;
     int i = 2;
+    while (i * i <= angka) {
+        if (angka % i == 0) return false;
+        i++;
+    }
+    return true;
 }
