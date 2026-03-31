0060058bbee5d22abf0dc271ad33145667910ca6
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 15:14:49 +0700

    pengecekan bilangan prima

--- loop.cpp
+++ loop.cpp
@@ -6,12 +6,7 @@ int angka;
 int pilihan;
 
 // cek bilangan prima
-bool cekPrima() {
+bool cekPrima(){
     if (angka < 2) return false;
     int i = 2;
-    while (i * i <= angka) {
-        if (angka % i == 0) return false;
-        i++;
-    }
-    return true;
 }
