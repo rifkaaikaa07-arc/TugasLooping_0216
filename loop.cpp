fc92b68b1028518f366f9dd52ffc8b70403e16f4
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 15:12:04 +0700

    menambahkan angka kurang dari 2

--- loop.cpp
+++ loop.cpp
@@ -4,3 +4,14 @@ using namespace std;
 // variabel global
 int angka;
 int pilihan;
+
+// cek bilangan prima
+bool cekPrima() {
+    if (angka < 2) return false;
+    int i = 2;
+    while (i * i <= angka) {
+        if (angka % i == 0) return false;
+        i++;
+    }
+    return true;
+}
